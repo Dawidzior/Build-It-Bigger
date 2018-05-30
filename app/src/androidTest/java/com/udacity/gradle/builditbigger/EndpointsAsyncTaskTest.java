577b83439868893ed106692acdb396862137e48c
Dawid <klembix@o2.pl>
2018-05-31 00:10:31 +0200

    Refactor

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,18 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest extends AndroidTestCase {
+
+    @Test
+    public void testEndpointsAsyncTask() {
+
+        //TODO
+        assert true;
+    }
+}
