26f0764a778c4aeb3a131fd0123a2edb0b64ad64
Dawid <klembix@o2.pl>
2018-05-31 12:43:05 +0200

    AsyncTask test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,18 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.AndroidTestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import pl.dawidzior.javajokerlib.Joker;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 @RunWith(AndroidJUnit4.class)
-public class EndpointsAsyncTaskTest extends AndroidTestCase {
-
+public class EndpointsAsyncTaskTest {
     @Test
-    public void testEndpointsAsyncTask() {
-
-        //TODO
-        assert true;
+    public void endpointsAsyncTaskJokeRetrieveTest() {
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+        String jokeFromAsync = endpointsAsyncTask.doInBackground(InstrumentationRegistry.getContext());
+        assertNotNull(jokeFromAsync);
+        assertEquals(jokeFromAsync, Joker.tellJoke());
     }
 }
